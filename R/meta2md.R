e7908869191da8006824f15b84795270df0a3662
qhu <qiang.hu@roswellpark.org>
2022-10-19 11:50:17 -0400

    init

--- R/meta2md.R
+++ R/meta2md.R
@@ -10,15 +10,28 @@ meta2md <- function(cwl, plotdir = "plots"){
 
     ## plot
     nn <- deparse(substitute(cwl))
+    if(is(cwl, "cwlWorkflow")){
+        mtime <- mcols(cwlSearch(paste0("pl_", nn)))$mtime
+    }else{
+        mtime <- mcols(cwlSearch(paste0("tl_", nn)))$mtime
+    }
+    
     dir.create(plotdir, showWarnings = FALSE)
     pl <- plotCWL(cwl)
     write(export_svg(pl), file.path(plotdir, paste0(nn, ".svg")))
 
     inputs <- paste(kable(do.call(rbind, mt$inputs)), collapse = "\n")
     outputs <- paste(kable(do.call(rbind, mt$outputs)), collapse = "\n")
-    md <- paste(title, des,
+
+    header <- paste("---", paste("title:", mt$label),
+                    paste("description:", des),
+                    paste("Author:", cwl@extensions$author),
+                    paste("Last updated:", sub("\\s.*", "", mtime)),
+                    "---", sep = "\n")
+    
+    md <- paste(header, title, des,
                 "## plot",
-                paste0("![Alt text](", file.path(plotdir, paste0(nn, ".svg")), ")"),
+                paste0("![", title, "](/", plotdir, "/",nn, ".svg", ")"),
                 "## Inputs", inputs,
                 "## Outputs", outputs, sep = "\n")
 
@@ -26,5 +39,6 @@ meta2md <- function(cwl, plotdir = "plots"){
         steps <- paste(kable(do.call(rbind, mt$steps)), collapse = "\n")
         md <- paste(md, "## steps", steps, sep = "\n")
     }
+    md <- gsub("character\\(0\\)", " ", md)
     return(md)
 }
