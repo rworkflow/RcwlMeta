9ccdfe267d9a6f7ff26440422b4bfb8e93febe96
Qian Liu <qliu7@buffalo.edu>
2022-11-14 12:51:21 -0500

    add 'type' column for input and output tables

--- R/meta2md.R
+++ R/meta2md.R
@@ -1,57 +1,72 @@
+#' meta2md
 #' Meta information to markdown
-#'
+#' @param cwl the `cwlProcess` or `cwlWorkflow` object
+#' @param outdir the output file path for the to-be-generated figure and md files
+#' @param prefix the filename for to-be-generated .svg file and .md file
 #' @importFrom DiagrammeRsvg export_svg
 #' @importFrom knitr kable
 #' @export
 
-meta2md <- function(cwl, plotdir = "plots"){
+meta2md <- function(cwl, outdir = ".", prefix = deparse(substitute(cwl))){
     mt <- meta(cwl)
     title <- paste("##", mt$label)
     des <- mt$doc
 
     mtime <- mt$extensions$`$rud`$date  ## for data recipe
 
-    nn <- deparse(substitute(cwl))
     if (is.null(mtime)) {
         if(is(cwl, "cwlWorkflow")){
-            mtime <- mcols(cwlSearch(paste0("pl_", nn)))$mtime
+            mtime <- mcols(cwlSearch(paste0("pl_", prefix)))$mtime
         }else{
-            mtime <- mcols(cwlSearch(paste0("tl_", nn)))$mtime
+            mtime <- mcols(cwlSearch(paste0("tl_", prefix)))$mtime
         }
     }
     ## plot
-    dir.create(plotdir, showWarnings = FALSE)
+    dir.create(outdir, showWarnings = FALSE)
     pl <- plotCWL(cwl)
-    write(export_svg(pl), file.path(plotdir, paste0(nn, ".svg")))
+    write(export_svg(pl), file.path(outdir, paste0(prefix, ".svg")))
 
-    inputs <- paste(knitr::kable(do.call(rbind, mt$inputs)), collapse = "\n")
-    outputs <- paste(knitr::kable(do.call(rbind, mt$outputs)), collapse = "\n")
+    ## table for inputs/outputs 
+    itypes <- lapply(inputs(cwl), function(x) paste(x@type, collapse=";"))
+    otypes <- lapply(outputs(cwl), function(x) paste(x@type, collapse=";"))
+    itable <- cbind(do.call(rbind, mt$inputs), do.call(rbind, itypes))
+    itable <- itable[, c(1,3,2), drop=FALSE]
+    otable <- cbind(do.call(rbind, mt$outputs), do.call(rbind, otypes))
+    otable <- otable[, c(1,3,2), drop=FALSE]
+    colnames(itable) <- colnames(otable) <- c("label", "type", "description")
+    inputs <- paste(knitr::kable(itable), collapse = "\n")
+    outputs <- paste(knitr::kable(otable), collapse = "\n")
 
+    ## header
     header <- paste("---", paste("title:", mt$label),
                     paste("description:", des),
                     paste("Author:", cwl@extensions$`$rud`$author),
                     paste("Last updated:", sub("\\s.*", "", mtime)),
+                    paste("type: article"),
                     "---", sep = "\n")
-
-    if(!is.null(mt$extensions$`$rud`$url)) {  ## add data source url for data recipe (ReUseData)
-        des <- paste0(des, "\n", "Data source: ", paste0("<", mt$extension$`$rud`$url, ">", collapse = "; "))
+    
+    ## add data source url for data recipe (ReUseData)
+    if(!is.null(mt$extensions$`$rud`$url)) { 
+        des <- paste0(des, "<br>Data source: ", paste0("<", mt$extension$`$rud`$url, ">", collapse = "; "))
     }
 
     md <- paste(header, title, des,
                 "## plot",
-                paste0("![", title, "](/", plotdir, "/",nn, ".svg", ")"),
+                paste0("![", title, "](/plots/",prefix, ".svg", ")"),
                 "## Inputs", inputs,
                 "## Outputs", outputs, sep = "\n")
 
+    ## add step info for workflow recipes (RcwlPipelines)
     if(!is.null(mt$steps)){
         steps <- paste(knitr::kable(do.call(rbind, mt$steps)), collapse = "\n")
         md <- paste(md, "## steps", steps, sep = "\n")
     }
     md <- gsub("character\\(0\\)", " ", md)
 
-    if(!is.null(mt$extension$`$rud`$example)) {  ## add example code for data recipe (ReUseData)
+    ## add example code for data recipes (ReUseData)
+    if(!is.null(mt$extension$`$rud`$example)) { 
         md <- paste(md, "## Example:", "```", mt$extension$`$rud`$example, "```\n", sep="\n")
     }
-    
+    write(md, file = file.path(outdir, paste0(prefix, ".md")))
     return(md)
 }
